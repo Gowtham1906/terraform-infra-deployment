553e7317ba81427e571fd557b978f3db770f9f49
S.GOWTHAM <88620210+Gowtham1906@users.noreply.github.com>
2024-09-04 13:21:23 +0530

    Update main.tf

--- terraform-s3/main.tf
+++ terraform-s3/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1" 
 }
 #make changes for testing
-resource "aws_s3_bucket" "my_bucket" {
+resource "aws_s" "my_bucket" {
   bucket = "my-infra-bucket-040924"
   acl    = "private"
 }
