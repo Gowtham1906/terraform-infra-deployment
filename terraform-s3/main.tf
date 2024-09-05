3ef0613ed897cf68343834e1d1f6c36ce9359ff9
S.GOWTHAM <88620210+Gowtham1906@users.noreply.github.com>
2024-09-05 16:12:10 +0530

    Update main.tf

--- terraform-s3/main.tf
+++ terraform-s3/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-infra-bucket-89076543wty4erdhesadfastwgwsw"
+  bucket = "my-infra-bucket-89076543wnhkfcugxdbvnewwwwwwww"
 
   tags = {
     Name        = "MyInfraBucket"
